b3f0017a8ec128ed618a91308e52da8b91bcab2e
Moses_of_Egypt <mosesbobadilla@gmail.com>
2026-07-15 18:11:33 -0400

    Implement NGP arm mimics

--- object_code/common/enemies/armMimic.s
+++ object_code/common/enemies/armMimic.s
@@ -44,7 +44,26 @@ armMimic_uninitialized:
 	call enemySetAnimation
 
 	ld a,SPEED_100
+.ifdef ENABLE_NEW_GAME_PLUS
+	call ecom_setSpeedAndState8AndVisible
+	ld hl,@ngpUpgradeTable
+	jp tryNgpUpgradeStrongEnemyIgnoreSubids
+.else
 	jp ecom_setSpeedAndState8AndVisible
+.endif
+
+.ifdef ENABLE_NEW_GAME_PLUS
+@ngpUpgradeTable:
+	.dw @ngpEnemyUpgrades
+	.dw @ngpEnemyUpgrades
+	.dw @ngpEnemyUpgrades
+
+@ngpEnemyUpgrades:
+	m_ngp_upgrade_p_d_h				PALETTE_GOLD      10  7
+	m_ngp_upgrade_p_d_h				PALETTE_RED       10  7
+	m_ngp_upgrade_p_si_d_h			PALETTE_BLUE   01 10  7
+	m_ngp_upgrade_p_si_d_h_s_term	PALETTE_GREEN  02 16 12 SPEED_140
+.endif
 
 
 armMimic_state_switchHook:
@@ -70,8 +89,51 @@ armMimic_state8:
 	; Check that Link is moving
 	ld a,(wLinkAngle)
 	inc a
-	ret z
+.ifdef ENABLE_NEW_GAME_PLUS
+	ld h,d
+	ld l,Enemy.subid
+	jr nz,+
+		ld a,(hl)
+		cp $02
+		ret nz
 
+		call objectGetAngleTowardLink
+		; update angle
+		ld h,d
+		ld l,Enemy.angle
+		ldd (hl),a
+
+		; update direction
+		sla a
+		swap a
+		and $03
+		ldd (hl),a
+
+		push af
+		ld e,Enemy.angle
+		call ecom_applyVelocityForSideviewEnemyNoHoles
+		pop af
+		ld h,d
+		ld l,Enemy.var30
+		jr ++
+	+
+	ld e,a
+	ld a,(hl)
+	or a
+	jr z,+
+		ld hl,wFrameCounter
+		xor a
+		bit 5,(hl)
+		jr z,+
+			add $08
+			bit 6,(hl)
+			jr z,+
+				add $10
+	+
+	add e
+.else
+	ret z
+.endif
 	add $0f
 	and $1f
 	ld e,Enemy.angle
@@ -83,6 +145,7 @@ armMimic_state8:
 	ld a,(w1Link.direction)
 	add $02
 	and $03
+	++
 	cp (hl)
 	jr z,@animate
 
